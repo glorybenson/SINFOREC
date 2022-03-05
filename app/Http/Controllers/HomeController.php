05b9fd7db882695d114da1da00c0d440afe10eaa
Glory Benson <glorybenson3738@gmail.com>
2022-03-05 03:32:57 -0800

    Revert "sssssss"
    This reverts commit 383ce97d811c76727ec08ae24d308b8deb22af1d.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,13 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
+use App\Models\Arrondissement;
+use App\Models\Centre;
+use Illuminate\Support\Facades\Route;
+use App\Models\Communes;
+use App\Models\Department;
+use App\Models\Pay;
+use App\Models\Region;
 use Illuminate\Support\Facades\Validator;
 
 
@@ -122,62 +129,183 @@ class HomeController extends Controller
     public function create()
     {
         $shell = new \stdClass();
-        $binding = Util::load( $shell);
-
-        return view('users.create', $binding);
+        $binding = Util::load($shell);
+        $data['pays'] = $r = Pay::all();
+        $data['regions'] = $r = Region::all();
+        $data['departments'] = Department::all();
+        $data['arrondissements'] = Arrondissement::all();
+        $data['communes'] = Communes::all();
+        $data['centre'] = Centre::all();
+        $array_data = [
+            'pays' => $data['pays'],
+            'regions' => $data['regions'],
+            'departments' => $data['departments'],
+            'arrondissements' => $data['arrondissements'],
+            'communes' => $data['communes'],
+            'centre' => $data['centre'],
+            'mode' => 'create'
+        ];
+        $data['datas'] = $array_data;
+        return view('users.create', $data);
     }
 
-    public function edit_user( $id)
+    public function edit_user($id)
     {
-        $shell = new \stdClass();
-        $binding = Util::load( $shell);
-        $user = DB::table( 'users')->where( 'id', $id)->get()[ 0];
-
-        return view( 'users.edit', array_merge( [ "user" => $user], $binding));
+        try {
+            $data['roles'] = [
+                ['id' => 2, 'name' => 'Administration'],
+                ['id' => 3, 'name' => 'Centres'],
+                ['id' => 4, 'name' => 'Mariage'],
+                ['id' => 5, 'name' => 'Décès'],
+                ['id' => 6, 'name' => 'Certificat De'],
+                ['id' => 7, 'name' => 'Rapports'],
+                ['id' => 8, 'name' => 'Tableau de Bord'],
+            ];
+            $data['user'] = $user = User::find($id);
+            if (!$user) {
+                Session::flash('warning', 'The User not found');
+                return redirect()->route('home');
+            }
+            $data['pays'] = $r = Pay::all();
+            $data['regions'] = $r = Region::all();
+            $data['departments'] = Department::all();
+            $data['arrondissements'] = Arrondissement::all();
+            $data['communes'] = Communes::all();
+            $data['centre'] = Centre::all();
+            $array_data = [
+                'pays' => $data['pays'],
+                'regions' => $data['regions'],
+                'departments' => $data['departments'],
+                'arrondissements' => $data['arrondissements'],
+                'communes' => $data['communes'],
+                'centre' => $data['centre'],
+            ];
+            $data['datas'] = $array_data;
+            if (Route::currentRouteName() == 'view.user') {
+                return view('users.view', $data);
+            }
+            return view('users.edit', $data);
+        } catch (\Throwable $th) {
+            //throw $th;
+            Session::flash('error', $th->getMessage());
+            return redirect()->route('home');
+        }
     }
 
-    public function update_user( Request $request)
+    public function update_user(Request $request)
     {
-        Util::try_save( $this->stash( $request), 'update');
+        try {
+            $rules = array(
+                'first_name' => ['required', 'string', 'max:255'],
+                'last_name' => ['required', 'string', 'max:255'],
+                'role' => ['required'],
+                'pays' => ['required'],
+                'regions' => ['required'],
+                'departments' => ['required'],
+                'arrondissements' => ['required'],
+                'communes' => ['required'],
+                'centres' => ['required'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $request->id],
+            );
 
-        return redirect( 'home')->with('success', 'Utilisateur créé avec succès');
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                Session::flash('warning', 'All fields are required');
+                return back()->withErrors($validator)->withInput();
+            }
+
+            $createUser = User::find($request->id);
+            $createUser->first_name = $request->first_name;
+            $createUser->last_name = $request->last_name;
+            $createUser->email = $request->email;
+            $createUser->roles = $request->role;
+            $createUser->pays = $request->pays;
+            $createUser->regions = $request->regions;
+            $createUser->departments = $request->departments;
+            $createUser->arrondissements = $request->arrondissements;
+            $createUser->communes = $request->communes;
+            $createUser->centres = $request->centres;
+            $createUser->save();
+            return redirect('home')->with('success', 'Utilisateur créé avec succès');
+        } catch (\Throwable $th) {
+            //throw $th;
+            Session::flash('error', $th->getMessage());
+            return back();
+        }
     }
 
-    public function save( Request $request)
+    public function save(Request $request)
     {
-//        $this->validate( $request, HomeController::VALIDATION_FIELDS);
+        try {
+            $rules = array(
+                'first_name' => ['required', 'string', 'max:255'],
+                'last_name' => ['required', 'string', 'max:255'],
+                'role' => ['required'],
+                'pays' => ['required'],
+                'regions' => ['required'],
+                'departments' => ['required'],
+                'arrondissements' => ['required'],
+                'communes' => ['required'],
+                'centres' => ['required'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+            );
 
-        $user = $this->stash( $request);
-        $user->save();
+            $validator = Validator::make($request->all(), $rules);
 
-//        Util::try_save( , 'save');
+            if ($validator->fails()) {
+                Session::flash('warning', 'All fields are required');
+                return back()->withErrors($validator)->withInput();
+            }
+            //dd($request->all());
 
-        return redirect( 'home')->with('success', 'Utilisateur créé avec succès');
+            $createUser = new User();
+            $createUser->first_name = $request->first_name;
+            $createUser->last_name = $request->last_name;
+            $createUser->email = $request->email;
+            $createUser->password = Hash::make($request->password);
+            $createUser->roles = $request->role;
+            $createUser->pays = $request->pays;
+            $createUser->regions = $request->regions;
+            $createUser->departments = $request->departments;
+            $createUser->arrondissements = $request->arrondissements;
+            $createUser->communes = $request->communes;
+            $createUser->centres = $request->centres;
+            $createUser->created_by = Auth::user()->id;
+            $createUser->timestamps = true;
+            $createUser->save();
+            return redirect('home')->with('success', 'Utilisateur créé avec succès');
+        } catch (\Throwable $th) {
+            //throw $th;
+            Session::flash('error', $th->getMessage());
+            return back();
+        }
     }
 
-    public function delete_user( $id)
+    public function delete_user($id)
     {
-        DB::delete( "DELETE FROM users WHERE id=?", [ $id]);
-        return back()->with( 'success', "Deleted entry successfully!");
+        DB::delete("DELETE FROM users WHERE id=?", [$id]);
+        return back()->with('success', "Deleted entry successfully!");
     }
 
-    private function stash( Request $request)
+    private function stash(Request $request)
     {
-//        $this->validate( $request, HomeController::VALIDATION_FIELDS);
+        //        $this->validate( $request, HomeController::VALIDATION_FIELDS);
 
         $createUser = new User();
 
-        Util::fill( $createUser, [ 'first_name', 'last_name', 'email', 'created_by'], $request);
-        $createUser->password = Hash::make( $request->password);
+        Util::fill($createUser, ['first_name', 'last_name', 'email', 'created_by'], $request);
+        $createUser->password = Hash::make($request->password);
         $createUser->roles = $request->role;
         $createUser->created_by = Auth::user()['id'];
         $createUser->timestamps = true;
-        $createUser->pays = json_encode( $request->pays);
-        $createUser->regions = json_encode( $request->regions);
-        $createUser->departments = json_encode( $request->departments);
-        $createUser->arrondissements = json_encode( $request->arrondissements);
-        $createUser->communes = json_encode( $request->communes);
-        $createUser->centres = json_encode( $request->centre);
+        $createUser->pays = json_encode($request->pays);
+        $createUser->regions = json_encode($request->regions);
+        $createUser->departments = json_encode($request->departments);
+        $createUser->arrondissements = json_encode($request->arrondissements);
+        $createUser->communes = json_encode($request->communes);
+        $createUser->centres = json_encode($request->centre);
 
         return $createUser;
     }
