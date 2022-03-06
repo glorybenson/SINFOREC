0202074ee8c6296a9430f63fcc494496a8fe0a71
Glory Benson <glorybenson3738@gmail.com>
2022-03-06 04:43:21 -0800

    1

--- resources/views/layouts/includes/script.blade.php
+++ resources/views/layouts/includes/script.blade.php
@@ -24,7 +24,7 @@
     $(document).ready(function() {
         $('select').selectpicker();
         $('table').DataTable();
-        // createCenterAutocomplete();
+        createCenterAutocomplete();
         setupDraft();
 
         if( location.pathname.indexOf( '/centre/create') !== -1
